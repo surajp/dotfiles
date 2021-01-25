c4eea353487aaac889c3f591e12bac17aa77a7a1
Suraj Pillai <85.suraj@gmail.com>
2021-01-25 08:10:59 -0500

    Restrict to sfdx command only and pass querystring

--- fzf-key-bindings.bash
+++ fzf-key-bindings.bash
@@ -81,11 +81,18 @@ __fzf_sfdx__(){
     local flag="$(__fzf_flags__ $subcmd)"
     if [[ "$flag" != "" ]]
     then
-      READLINE_LINE="$fullcmd --$flag"
-      READLINE_POINT=$(( ${#fullcmd} + ${#flag} + 3 ))
+      READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}--$flag${READLINE_LINE:$READLINE_POINT}"
+      READLINE_POINT=$(( READLINE_POINT + ${#flag} + 3 ))
+      #READLINE_LINE="$fullcmd --$flag"
+      #READLINE_POINT=$(( ${#fullcmd} + ${#flag} + 3 ))
     fi
-  else
-    local selected="$(cat ~/.sfdxcommands.json | jq -r '.[].id' | $(__fzfcmd) +m --bind 'ctrl-z:ignore,alt-j:preview-down,alt-k:preview-up' --preview='cat ~/.sfdxcommands.json | jq -r ".[] | select (.id==\""{}"\") | [\"\nDescription:\n \"+.description,\"\nUsage:\n \"+select(has(\"usage\")).usage, \"\nExamples:\n \"+(select(has(\"examples\")).examples|join(\"\n\"))][]"' --preview-window='right:wrap')"
+  elif [[ "$cmd" == "sfdx" || "$cmd" == "" ]]
+  then
+    local querystr=""
+    if [[ "$subcmd" != "" ]]; then
+     querystr="--query $subcmd" 
+    fi
+    local selected="$(cat ~/.sfdxcommands.json | jq -r '.[].id' | $(__fzfcmd) +m --bind 'ctrl-z:ignore,alt-j:preview-down,alt-k:preview-up' --preview='cat ~/.sfdxcommands.json | jq -r ".[] | select (.id==\""{}"\") | [\"\nDescription:\n \"+.description,\"\nUsage:\n \"+select(has(\"usage\")).usage, \"\nExamples:\n \"+(select(has(\"examples\")).examples|join(\"\n\"))][]"' --preview-window='right:wrap' $querystr)"
     if [[ "$selected" != "" ]]; then
       READLINE_LINE="sfdx $selected"
       READLINE_POINT=$(( 5 + ${#selected} ))
@@ -134,9 +141,9 @@ else
   bind -m vi-command -x '"\C-e": __fzf_emojis__'
   bind -m vi-insert -x '"\C-e": __fzf_emojis__'
 
-  bind -m emacs-standard -x '"\C-k": __fzf_sfdx__'
-  bind -m vi-command -x '"\C-k": __fzf_sfdx__'
-  bind -m vi-insert -x '"\C-k": __fzf_sfdx__'
+  bind -m emacs-standard -x '"\C-l": __fzf_sfdx__'
+  bind -m vi-command -x '"\C-l": __fzf_sfdx__'
+  bind -m vi-insert -x '"\C-l": __fzf_sfdx__'
 
 fi
 
