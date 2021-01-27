b82ca3121d1532487fe86545c9659100aea2dc0b
Suraj Pillai <85.suraj@gmail.com>
2021-01-26 19:31:59 -0500

    soql editor

--- fzf-key-bindings.bash
+++ fzf-key-bindings.bash
@@ -65,6 +65,20 @@ __fzf_emojis__(){
   READLINE_POINT=$(( READLINE_POINT + ${#selectedSmiley} ))
 }
 
+__fzf_soql(){
+  if [[ -f "./schema.txt" ]]; then
+    local selected="$(cat ./schema.txt | $(__fzfcmd) | awk -F ' ' '{print $1}')"
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
+    READLINE_POINT=$(( READLINE_POINT + ${#selected} ))
+  fi
+}
+
+__fzf_sfdx_alias(){
+  local selected="$(cat ~/.sfdxaliases | $(__fzfcmd) | awk '{print $2}')"
+  READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
+  READLINE_POINT=$(( READLINE_POINT + ${#selected} ))
+}
+
 __fzf_sfdx_flags__(){
   local selected="$1"
   local fullcmd=""
@@ -141,13 +155,21 @@ else
   bind -m vi-command -x '"\C-r": __fzf_history__'
   bind -m vi-insert -x '"\C-r": __fzf_history__'
 
-  bind -m emacs-standard -x '"\C-e": __fzf_emojis__'
-  bind -m vi-command -x '"\C-e": __fzf_emojis__'
-  bind -m vi-insert -x '"\C-e": __fzf_emojis__'
+  bind -m emacs-standard -x '"\C-3": __fzf_emojis__'
+  bind -m vi-command -x '"\C-3": __fzf_emojis__'
+  bind -m vi-insert -x '"\C-3": __fzf_emojis__'
 
-  bind -m emacs-standard -x '"\C-l": __fzf_sfdx__'
-  bind -m vi-command -x '"\C-l": __fzf_sfdx__'
-  bind -m vi-insert -x '"\C-l": __fzf_sfdx__'
+  bind -m emacs-standard -x '"\C-e": __fzf_sfdx__'
+  bind -m vi-command -x '"\C-e": __fzf_sfdx__'
+  bind -m vi-insert -x '"\C-e": __fzf_sfdx__'
+
+  bind -m emacs-standard -x '"\C-n": __fzf_sfdx_alias'
+  bind -m vi-command -x '"\C-n": __fzf_sfdx_alias'
+  bind -m vi-insert -x '"\C-n": __fzf_sfdx_alias'
+
+  bind -m emacs-standard -x '"\C-y": __fzf_soql'
+  bind -m vi-command -x '"\C-q": __fzf_soql'
+  bind -m vi-insert -x '"\C-q": __fzf_soql'
 
 fi
 
--- refreshvimdict.sh
+++ refreshvimdict.sh
@@ -9,17 +9,29 @@ then
   rm schema.txt
 fi
 
+sfdx force:schema:sobject:list -c custom > custom_objects.txt
+
 # We can't query all standard fields in a single tooling api query. Hence, we loop. Add/remove objects to 'standard_objects.txt' as required
 while read objectname
 do
-  sfdx force:data:soql:query -q "select developername from entityparticle where entitydefinitionid='$objectname'" -t -r csv >> schema.txt
+  sfdx force:data:soql:query -q "select name,entitydefinitionid from entityparticle where entitydefinitionid='$objectname'" -t -r csv >> schema.txt
 done < standard_objects.txt
 
+while read objectname
+do
+  sfdx force:data:soql:query -q "select name,entitydefinitionid from entityparticle where entitydefinitionid='$objectname'" -t -r csv >> schema.txt
+  sed -i "s/01I[^ ]*/$objectname/g" schema.txt
+done < custom_objects.txt
+
 # Get custom fields
-sfdx force:data:soql:query -q "Select developername from customfield" -t -r csv >> schema.txt
+# sfdx force:data:soql:query -q "Select developername,TableEnumOrId from customfield" -t -r csv >> schema.txt
 
 #sort and uniq in place
 sort -u -o schema.txt schema.txt
+
+cat schema.txt | awk -F ',' '{len=length($1);i=0;while(i<40-len){$1=$1" ";i++};print $1$2}' > schema1.txt
+
+mv schema1.txt schema.txt
 
 #add schema.txt to your vim dictionary using
 #set dictionary+=<your pwd path>/schema.txt
