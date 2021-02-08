72c2885c46692c807751f72784af70f25e7e5456
Suraj Pillai <suraj.pillai@vertexcs.com>
2021-02-07 21:38:29 -0500

    Fix issue with multiple sfdx flag selection

--- fzf-key-bindings.bash
+++ fzf-key-bindings.bash
@@ -97,7 +97,7 @@ __fzf_sfdx_flags__(){
   do fullcmd+=" $i"
   done
   local ret=`cat ~/.sfdxcommands.json | jq -r ".[] | select(.id==\"$selected\") | .flags | keys[]" | $(__fzfcmd) -m --bind 'ctrl-z:ignore,alt-j:preview-down,alt-k:preview-up' --preview='cat ~/.sfdxcommands.json | jq -r ".[] | select(.id==\"'$selected'\") | .flags | to_entries[] | select (.key==\""{}"\") | [\"Command:\n'"$fullcmd"'\n\",\"Flag Description:\",.value][]"' --preview-window='right:wrap'`
-  echo $ret
+  echo "${ret//$'\n'/ --}"
 }
 
 __fzf_sfdx__(){
