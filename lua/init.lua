4bcde3af9bdc1c451093cc7a15f10b6bc2794118
Suraj Pillai <85.suraj@gmail.com>
2023-10-08 03:32:58 -0400

    Ensure fzf find works in oil file explorer

--- lua/init.lua
+++ lua/init.lua
@@ -1,4 +1,8 @@
 local opts = {noremap=true,silent=true}
 
-require("oil").setup()
+require("oil").setup({
+  keymaps={
+    ["<C-p>"]=":Files!<CR>"
+  }
+})
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
