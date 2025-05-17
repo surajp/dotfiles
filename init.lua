7efff75a8b4161a498e81adf43213e48fac551d0
Suraj Pillai <85.suraj@gmail.com>
2025-05-17 11:22:00 -0400

    Change mapping for `Commands` in init.lua

--- init.lua
+++ init.lua
@@ -537,6 +537,7 @@ map("n", "<C-f>r", "<plug>(ale_find_references)", opts) -- ALE find references
 map("n", "<C-f>t", "<Cmd>Filetypes!<CR>", opts)
 map("n", "<C-f>o", "<Cmd>Colors<CR>", opts)
 map("n", "<C-f>x", "<Cmd>Commands<CR>", opts)
+map("n", "<leader>x", "<Cmd>Commands<CR>", opts)
 
 
 -- FZF Insert Mode Completions (using expr = true)
